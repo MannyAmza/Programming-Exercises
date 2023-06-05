6266224bc23a8b37248386f0aa778cb0cdf79938
MannyAmza <mannyamza@gmail.com>
2023-06-05 10:27:30 -0700

    Exercise 1.2

---
+++ Part 1 - Fundamentals of Programming/Chapter 1/Ex 1.2/Ex1_2.java
@@ -0,0 +1,10 @@
+//(Display five messages) Write a program that displays I love Java five times.
+public class Ex1_2 {
+    public static void main(String[] args){
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+    }
+}
